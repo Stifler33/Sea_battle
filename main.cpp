62b27a7bee7f3df8cfc66c6fca5f8fd3a69958ce
stifler33 <bagi33.ru@gmail.com>
2021-04-10 19:34:57 +0300

    modified

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(){
     bool middleShip[] = {true,true,true};
     bool smallShip[] = {true,true};
     bool tinyShip[] = {true};
-    cout << sizeof(bigShip) << endl;
-    cout << sizeof(middleShip) << endl;
-    cout << sizeof(smallShip) << endl;
-    cout << sizeof(tinyShip) << endl;
+    cout << sizeof(bigShip) / sizeof(bigShip[0])<< endl;
+    cout << sizeof(middleShip) / sizeof(middleShip[0]) << endl;
+    cout << sizeof(smallShip) / sizeof(smallShip[0])<< endl;
+    cout << sizeof(tinyShip) / sizeof(tinyShip[0])<< endl;
 }
